a7164c987644dc9484530f1f683df0be2216f74a
sidhyaashu <ashutoshsidhya69@gmail.com>
2024-10-31 22:13:52 +0530

    auth added

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 import app from "./src/app.js";
+import dotenv from "dotenv";
+import connectDB from "./src/config/db-config.js";
 
+dotenv.config();
 
-const PORT = process.env.PORT || 5000
+// Set default port if not provided in environment variables
+const PORT = process.env.PORT || 5000;
 
-
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`)
-})
+// Connect to database and start server
+(async () => {
+    try {
+        await connectDB();
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    } catch (error) {
+        console.error(`Error connecting to the database: ${error.message}`);
+        process.exit(1); 
+    }
+})();
--- src/app.js
+++ src/app.js
@@ -3,17 +3,16 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import helmet from "helmet";
-import connectDB from "./config/db-config.js";
 import { errorHandler } from "./middleware/error-handler-middleware.js";
 import { errorLogger } from "./middleware/error-logger-middleware.js";
 import { performanceMonitor } from "./middleware/performance-monitor-middleware.js";
 import { limiter } from "./middleware/rate-limiter-middleware.js";
 import { requestLogger } from "./middleware/request-logger-middleware.js";
 import authRoutes from "./routes/auth-routes.js"
+import hospitalRoute from "./routes/hospital-routes.js"
 
 // ========================================CONFIGARATIONS========================================
 dotenv.config();
-connectDB();
 const app = express();
 
 // ==========================================MIDDLEWARE==========================================
@@ -30,6 +29,7 @@ app.use(limiter)
 // ============================================ROUTES============================================
 
 app.use("/api/auth",authRoutes)
+app.use("/api/hospital",hospitalRoute)
 
 // =============================================HANDLERS=============================================
 app.use(errorHandler);
